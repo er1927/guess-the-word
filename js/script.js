dcc9b60116035581f5d87705072482ad8e8cb708
Elizabeth R <elizabeth.roberts1927@gmail.com>
2022-11-28 16:46:09 -0300

    Added rules for input and validator.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-const guessedLetters = document.querySelector("guessed-letters");
-const guessBttn = document.querySelector("guess");
-const letterInput = document.querySelector("letter");
-const wordInProgress = document.querySelector("word-in-progress");
-const remainingLetters = document.querySelector("remaining");
-const mssg = document.querySelector("message");
-const playAgainBttn = document.querySelector("play-again");
+const guessedLettersElements = document.querySelector(".guessed-letters");
+const guessBttn = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingLetters = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span")
+const mssg = document.querySelector(".message");
+const playAgainBttn = document.querySelector(".play-again");
 
-const word = "Magnolia";
+const word = "magnolia";
+const guessedLetters = [];
 
 const updateWordInProgress = function (word) {
     const updateWordInProgressLetters = [];
@@ -14,7 +16,6 @@ const updateWordInProgress = function (word) {
         console.log(letter);
         updateWordInProgressLetters.push("●");
     }
-
     wordInProgress.innerText = updateWordInProgressLetters.join("");
 };
 
@@ -22,7 +23,39 @@ updateWordInProgress(word);
 
 guessBttn.addEventListener("click", function (e) {
     e.preventDefault();
-    const inputValue = input.value;
-    console.log(inputValue);
-    input.value = "";
-})
+    mssg.innerText = "";
+    const guess = letterInput.value;
+    const rightGuess = validator(guess);
+    //console.log(rightGuess);
+    if (rightGuess) {
+        makeGuess(guess);
+    }
+    letterInput.value = "";
+});
+
+const validator = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0 ) {
+        mssg.innerText = "Type in a letter, please!";
+    }
+    else if (input.length > 1) {
+        mssg.innerText = "Type in ONE letter, please!";
+    }
+    else if (!input.match(acceptedLetter)) {
+        mssg.innerText = "Type in a letter from A to Z, please!";
+    }
+    else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        mssg.innerText = "You already guessed that one, try again!"
+    }
+    else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
